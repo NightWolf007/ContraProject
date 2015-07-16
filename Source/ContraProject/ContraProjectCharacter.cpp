e1446d278da2617c80e6045aa0b9874bc365b172
WhiteWolf4768 <ser4768@ya.ru>
2015-07-16 19:10:47 +0300

    nyancat

--- Source/ContraProject/ContraProjectCharacter.cpp
+++ Source/ContraProject/ContraProjectCharacter.cpp
@@ -1,4 +1,4 @@
-уси// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 #include "ContraProject.h"
 #include "ContraProjectCharacter.h"
