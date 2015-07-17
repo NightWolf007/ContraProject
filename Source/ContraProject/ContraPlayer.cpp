449fd954918d392f39ae897041b7526a2f85cb50
NightWolf <dimon19960121@gmail.com>
2015-07-17 18:23:26 +0300

    May be working

--- Source/ContraProject/ContraPlayer.cpp
+++ Source/ContraProject/ContraPlayer.cpp
@@ -2,28 +2,90 @@
 
 #include "ContraProject.h"
 #include "ContraPlayer.h"
-
+#include "PaperFlipbookComponent.h"
 
 // Sets default values
 AContraPlayer::AContraPlayer()
 {
+	// Setup the assets
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> RunningAnimationAsset;
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> AimRunningUpAnimationAsset;
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> AimRunningDownAnimationAsset;
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> IdleAnimationAsset;
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> StandAnimationAsset;
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> JumpAnimationAsset;
+		ConstructorHelpers::FObjectFinderOptional<UPaperFlipbook> DefeatAnimationAsset;
+		FConstructorStatics()
+			: RunningAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/Running.Running")),
+			AimRunningUpAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/AimRunningUp.AimRunningUp")),
+			AimRunningDownAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/AimRunningDown.AimRunningDown")),
+			IdleAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/Idle.Idle")),
+			StandAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/Stand.Stand")),
+			JumpAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/Jump.Jump")),
+			DefeatAnimationAsset(TEXT("/Game/2dSideScroller/Animation/Ray/Defeat.Defeat"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
+
+	RunningAnimation = ConstructorStatics.RunningAnimationAsset.Get();
+	AimRunningUpAnimation = ConstructorStatics.AimRunningUpAnimationAsset.Get();
+	AimRunningDownAnimation = ConstructorStatics.AimRunningDownAnimationAsset.Get();
+	IdleAnimation = ConstructorStatics.IdleAnimationAsset.Get();
+	StandAnimation = ConstructorStatics.StandAnimationAsset.Get();
+	JumpAnimation = ConstructorStatics.JumpAnimationAsset.Get();
+	DefeatAnimation = ConstructorStatics.DefeatAnimationAsset.Get();
+
+	GetSprite()->SetFlipbook(IdleAnimation);
+
+	// Use only Yaw from the controller and ignore the rest of the rotation.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
+	// Set the size of our collision capsule.
+	GetCapsuleComponent()->SetCapsuleHalfHeight(96);
+	GetCapsuleComponent()->SetCapsuleRadius(40);
+
+
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// PrimaryActorTick.bCanEverTick = true;
 
-}
+	// AutoPossessPlayer = EAutoReceiveInput::Player0;
 
-// Called when the game starts or when spawned
-void AContraPlayer::BeginPlay()
-{
-	Super::BeginPlay();
-	
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	camera->AttachTo(RootComponent);
+	camera->SetRelativeLocation(FVector(0, -200, 0));
+	camera->SetRelativeRotation(FRotator(0, 0, 0));
+	camera->ProjectionMode = ECameraProjectionMode::Orthographic;
+	camera->OrthoWidth = 2048;
+
+	// Configure character movement
+	GetCharacterMovement()->GravityScale = 2.0f;
+	GetCharacterMovement()->AirControl = 0.80f;
+	GetCharacterMovement()->JumpZVelocity = 1000.f;
+	GetCharacterMovement()->GroundFriction = 3.0f;
+	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	GetCharacterMovement()->MaxFlySpeed = 600.0f;
+
+	// Lock character motion onto the XZ plane, so the character can't move in or out of the screen
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->SetPlaneConstraintNormal(FVector(0.0f, -1.0f, 0.0f));
+
+	GetCharacterMovement()->bUseFlatBaseForFloorChecks = true;
 }
 
 // Called every frame
-void AContraPlayer::Tick( float DeltaTime )
+void AContraPlayer::UpdateAnimation()
 {
-	Super::Tick( DeltaTime );
-
+	if (GetVelocity().Size() > 0)
+		GetSprite()->SetFlipbook(RunningAnimation);
+	else
+		GetSprite()->SetFlipbook(IdleAnimation);
 }
 
 // Called to bind functionality to input
@@ -31,5 +93,13 @@ void AContraPlayer::SetupPlayerInputComponent(class UInputComponent* InputCompon
 {
 	Super::SetupPlayerInputComponent(InputComponent);
 
+	InputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	InputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+
+	InputComponent->BindAxis("Move", this, &AContraPlayer::Move);
 }
 
+void AContraPlayer::Move(float AxisValue)
+{
+	AddMovementInput(FVector(1, 0, 0), AxisValue);
+}
--- Source/ContraProject/ContraPlayer.h
+++ Source/ContraProject/ContraPlayer.h
@@ -2,27 +2,42 @@
 
 #pragma once
 
-#include "GameFramework/Character.h"
+#include "PaperCharacter.h"
 #include "ContraPlayer.generated.h"
 
 UCLASS()
-class CONTRAPROJECT_API AContraPlayer : public ACharacter
+class CONTRAPROJECT_API AContraPlayer : public APaperCharacter
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this character's properties
 	AContraPlayer();
-
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 	
 	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
+	void UpdateAnimation();
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	
-	
+	void Move(float AxisValue);
+
+protected:
+	UPROPERTY(EditAnywhere)
+		class UCameraComponent* camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* RunningAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* AimRunningUpAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* AimRunningDownAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* IdleAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* StandAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* JumpAnimation;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Animations)
+		class UPaperFlipbook* DefeatAnimation;
 };
